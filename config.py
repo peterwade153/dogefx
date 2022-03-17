684114026cf7f2f15b4c5949223b48cd22c31839
peterwade153 <peterwade153@gmail.com>
2022-03-17 12:15:48 +0300

    Update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings, AnyUrl
 
 class Settings(BaseSettings):
     api_key: str = os.getenv("API_KEY")
-    base_api_key: str = os.getenv("BASE_API_URL")
-    redis_url: str = os.getenv("REDIS_URL")
+    base_api_key: AnyUrl = os.getenv("BASE_API_URL")
+    redis_url: AnyUrl = os.getenv("REDIS_URL")
 
 settings = Settings()
